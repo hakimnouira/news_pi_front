481ce67d1b111c9af25180746580bfacfa997375
Aziz7905 <114614709+Aziz7905@users.noreply.github.com>
2025-11-08 21:43:56 +0100

    Geo-Rag-Game

--- compassnet/src/app/app.routes.ts
+++ compassnet/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Main } from './components/main/main';
 import { Signup } from './components/signup/signup';
 import { Signin } from './components/signin/signin';
 import { Antiprop } from './components/antiprop/antiprop';
+import { GeoRagGame } from './components/geo-rag-game/geo-rag-game';
 import { Home } from './components/home/home';
 
 export const routes: Routes = [
@@ -11,7 +12,8 @@ export const routes: Routes = [
     component: Main,
     children: [
       { path: '', component: Home },
-      { path: 'isitpropaganda', component: Antiprop }
+      { path: 'isitpropaganda', component: Antiprop },
+      { path: 'geo-rag-game', component: GeoRagGame },
     ],
   },
   { path: 'signup', component: Signup },  // Top-level: /signup
