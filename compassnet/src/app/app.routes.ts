7df53741caff738fa94b29a5f422224d5a47c785
unknown <nouira.hakimo@gmail.com>
2025-11-09 14:24:47 +0100

    news check

--- compassnet/src/app/app.routes.ts
+++ compassnet/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Signin } from './components/signin/signin';
 import { Antiprop } from './components/antiprop/antiprop';
 import { GeoRagGame } from './components/geo-rag-game/geo-rag-game';
 import { Home } from './components/home/home';
+import { NewsCheck } from './components/news-check/news-check';
 
 export const routes: Routes = [
     {
@@ -14,6 +15,7 @@ export const routes: Routes = [
       { path: '', component: Home },
       { path: 'isitpropaganda', component: Antiprop },
       { path: 'geo-rag-game', component: GeoRagGame },
+      { path: 'news-check', component: NewsCheck },
     ],
   },
   { path: 'signup', component: Signup },  // Top-level: /signup
